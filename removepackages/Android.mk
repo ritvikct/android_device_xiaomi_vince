60fb86790bfcfbaeb6292e8cb3b6f2c6622b857c
ritvikct <ritvikct@proton.me>
2024-12-13 09:29:06 +0000

    vince: remove more packages
    Signed-off-by: ritvikct <ritvikct@proton.me>

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Aperture Chrome
+LOCAL_OVERRIDES_PACKAGES := Aperture Chrome Maps Drive Photos Gmail Markup MusicFX Youtube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
