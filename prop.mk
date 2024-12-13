79f0924c6897c9e6666f4769c4903ddf6eb518dd
ritvikct <ritvikct@proton.me>
2024-12-13 10:54:00 +0000

    vince: improve ram management

--- prop.mk
+++ prop.mk
@@ -363,6 +363,19 @@ wifi.interface=wlan0
 PRODUCT_SYSTEM_PROPERTIES += \
 media.wfd.max_resolution=7
 
+# Better Ram
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+
+
 # Zygote
 PRODUCT_SYSTEM_PROPERTIES += \
 zygote.critical_window.minute=10
